f2661bd5390686e2322c06ae0b821ed1318a21b9
alanantedoro <alanantedoro@gmail.com>
2021-09-27 15:05:31 -0300

    funcionalidades nuevas

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 
+const Contenedor = require('./Contenedor');
+const contenedor = new Contenedor('./productos.json')
+
 const app = express();
 
 const PORT = 8080;
 
-app.get('/', (req, res, next) => {
-    res.send({ mensaje: 'HOLA MUNDO'})
+const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+//Routing
+app.get('/', (req, res) => {
+    res.send({ mensaje: '5. Servidores Web'})
 });
 
+app.get('/productos', async (req, res) => {
+    const productos = await contenedor.getAll();
+    console.log('productos: ', productos);
+    res.json( productos );
+});
+
+app.get('/productoRandom', async (req, res) => {
+    const productos = await contenedor.getAll();
+    let random = getRandom(0, productos.length - 1);
+    console.log(random);
+    res.json( productos[random] );
+});
+
+
+
+//Server init y error
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`);
 });
